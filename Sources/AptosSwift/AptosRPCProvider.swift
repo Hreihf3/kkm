746e661e29ed06cd6e2bf5207d76da9f66737acd
xueyuejie <358838943@qq.com>
2022-08-03 14:29:31 +0800

    Update AptosRPCProvider.swift

--- Sources/AptosSwift/AptosRPCProvider.swift
+++ Sources/AptosSwift/AptosRPCProvider.swift
@@ -29,25 +29,7 @@ public struct AptosRPCProvider {
 }
 
 extension AptosRPCProvider {
-//    func GET<T:Codable>(url:String) -> Promise<T> {
-//        let (promise,seal) = Promise<T>.pending()
-//        AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil).responseData { response in
-//            switch response.result {
-//            case .success(let data):
-//                do {
-//                    let decoder = JSONDecoder()
-//                    decoder.keyDecodingStrategy = .convertFromSnakeCase
-//                    let result = try decoder.decode(T.self, from: data)
-//                    seal.fulfill(result)
-//                } catch let e{
-//                    seal.reject(e)
-//                }
-//            case let .failure(e):
-//                seal.reject(e)
-//            }
-//        }
-//        return promise
-//    }
+    
     public func GET<T: Codable>(url:String) -> Promise<T> {
        let rp = Promise<Data>.pending()
        var task: URLSessionTask? = nil
