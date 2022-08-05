2059898e656e8229725530de694726814455abcf
xueyuejie <358838943@qq.com>
2022-08-05 13:40:43 +0800

    fix

--- Sources/AptosSwift/AptosRPCProvider.swift
+++ Sources/AptosSwift/AptosRPCProvider.swift
@@ -22,7 +22,7 @@ public struct AptosRPCProvider {
         return self.POST(url: "https://faucet.devnet.aptoslabs.com/mint?amount=0&address=\(address.replacingOccurrences(of: "0x", with: ""))", parameters: [] as! [String])
     }
     
-    public func getAccount(address:String) -> Promise<AccountResult> {
+    public func getAccount(address:String) -> Promise<[AccountResult]> {
         return self.GET(url: "\(nodeUrl)/accounts/\(address)")
     }
     
