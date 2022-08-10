29cabb91a4c9bcdf666b1b369d207e20e6954cfb
xueyuejie <358838943@qq.com>
2022-08-10 10:50:24 +0800

    Update AptosRpcResult.swift

--- Sources/AptosSwift/Providers/AptosRpcResult.swift
+++ Sources/AptosSwift/Providers/AptosRpcResult.swift
@@ -7,44 +7,44 @@
 
 import Foundation
 //chaininfo
-public struct ChainInfo:Codable {
-    public let chainId:Int
-    public let epoch:String
-    public let ledgerVersion:String
-    public let oldestLedgerVersion:String
-    public let ledgerTimestamp:String
-    public let nodeRole:String
+public struct ChainInfo: Codable {
+    public let chainId: Int
+    public let epoch: String
+    public let ledgerVersion: String
+    public let oldestLedgerVersion: String
+    public let ledgerTimestamp: String
+    public let nodeRole: String
 }
 //AccountResult
-public struct AccountResult:Codable {
-    public let sequenceNumber:String
-    public let authenticationKey:String
+public struct AccountResult: Codable {
+    public let sequenceNumber: String
+    public let authenticationKey: String
 }
 //AccountResource
-public struct AccountResource:Codable {
-    public let type:String
-    public let data:AccountResourceData
+public struct AccountResource: Codable {
+    public let type: String
+    public let data: AccountResourceData
 }
 
-public struct AccountResourceData:Codable {
-    public let coin:DataCoin?
-    public let couter:String?
-    public let depositEvents:DataEvents?
-    public let withdrawEvents:DataEvents?
+public struct AccountResourceData: Codable {
+    public let coin: DataCoin?
+    public let couter: String?
+    public let depositEvents: DataEvents?
+    public let withdrawEvents: DataEvents?
     
-    public struct DataCoin:Codable {
-        public let value:String
+    public struct DataCoin: Codable {
+        public let value: String
     }
     
-    public struct DataEvents:Codable {
-        public let counter:String
-        public let guid:DataEventsGuid
+    public struct DataEvents: Codable {
+        public let counter: String
+        public let guid: DataEventsGuid
         
-        public struct DataEventsGuid:Codable {
-            public let id:guidId
-            public struct guidId:Codable {
-                public let addr:String
-                public let creationNum:String
+        public struct DataEventsGuid: Codable {
+            public let id: guidId
+            public struct guidId: Codable {
+                public let addr: String
+                public let creationNum: String
             }
         }
     }
