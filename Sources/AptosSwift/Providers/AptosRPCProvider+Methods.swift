2f0d88edbb3e4694c28c2ea1d5cc894b7aaa40a3
Forrest <zyyu1990@gmail.com>
2022-08-16 09:54:20 +0800

    Rest v1

--- Sources/AptosSwift/Providers/AptosRPCProvider+Methods.swift
+++ Sources/AptosSwift/Providers/AptosRPCProvider+Methods.swift
@@ -10,19 +10,19 @@ import PromiseKit
 
 extension AptosRPCProvider {
     public func getChainInfo() -> Promise<AptosRPC.ChainInfo> {
-        return GET()
+        return GET(path: "/v1")
     }
     
     public func getAccountData(address: AptosAddress) -> Promise<AptosRPC.AccountData> {
-        return GET(path: "/accounts/\(address.address)")
+        return GET(path: "/v1/accounts/\(address.address)")
     }
     
     public func getAccountResources(address: AptosAddress) -> Promise<[AptosRPC.AccountResource]> {
-        return GET(path: "/accounts/\(address.address)/resources")
+        return GET(path: "/v1/accounts/\(address.address)/resources")
     }
     
     public func getAccountResource(address: AptosAddress, resourceType: String) -> Promise<AptosRPC.AccountResource> {
-        return GET(path: "/accounts/\(address.address)/resource/\(resourceType)")
+        return GET(path: "/v1/accounts/\(address.address)/resource/\(resourceType)")
     }
     
     /// Submits a signed transaction to the the endpoint that takes BCS payload
@@ -30,7 +30,7 @@ extension AptosRPCProvider {
     /// - Returns: Transaction that is accepted and submitted to mempool
     public func submitSignedTransaction(_ signedTransaction: AptosSignedTransaction) -> Promise<AptosRPC.PendingTransaction> {
         let headers: [String: String] = ["Content-Type": "application/x.aptos.signed_transaction+bcs"]
-        return POST(path: "/transactions", body: try? BorshEncoder().encode(signedTransaction), headers: headers)
+        return POST(path: "/v1/transactions", body: try? BorshEncoder().encode(signedTransaction), headers: headers)
     }
     
     /// Submits a transaction to the the endpoint that takes BCS payload
@@ -40,6 +40,6 @@ extension AptosRPCProvider {
     public func simulateTransaction(_ rawTransaction: AptosRawTransaction, publicKey: AptosPublicKeyEd25519) -> Promise<[AptosRPC.UserTransaction]> {
         let signedTransaction = rawTransaction.simulate(publicKey)
         let headers: [String: String] = ["Content-Type": "application/x.aptos.signed_transaction+bcs"]
-        return POST(path: "/transactions/simulate", body: try? BorshEncoder().encode(signedTransaction), headers: headers)
+        return POST(path: "/v1/transactions/simulate", body: try? BorshEncoder().encode(signedTransaction), headers: headers)
     }
 }
