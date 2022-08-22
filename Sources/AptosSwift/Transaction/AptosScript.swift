6b4bcb5b93e2c6f2fc0c080092beaa38e09cf991
Forrest <zyyu1990@gmail.com>
2022-08-22 09:50:48 +0800

    bugfix

--- Sources/AptosSwift/Transaction/AptosScript.swift
+++ Sources/AptosSwift/Transaction/AptosScript.swift
@@ -7,51 +7,39 @@
 
 import Foundation
 
-public struct AptosEntryFunction {
-    public let moduleName: AptosModuleId
-    public let functionName: AptosIdentifier
+public struct AptosScript {
+    public let code: Data
     public let typeArgs: [AptosTypeTag]
-    public let args: [Data]
+    public let args: [AptosTransactionArgument]
     
     /// Scripts contain the Move bytecodes payload that can be submitted to Aptos chain for execution.
     /// - Parameters:
-    ///   - moduleName: Fully qualified module name in format "AccountAddress::module_name" e.g. "0x1::coin"
-    ///   - functionName: Function name
-    ///   - typeArgs: Type arguments that move function requires..
-    ///   - args: Arugments to the move function.
+    ///   - code: Move bytecode
+    ///   - typeArgs: Type arguments that bytecode requires.
+    ///   - args: Arugments to bytecode function.
     ///   @example
-    ///     A coin transfer function has three arugments "from", "to" and "amount".
+    ///     A coin transfer function has one type argument "CoinType".
     ///     ```
     ///     public(script) fun transfer<CoinType>(from: &signer, to: address, amount: u64,)
     ///     ```
-    /// - Returns: Aptos EntryFunction
-    public init(moduleName: AptosModuleId, functionName: AptosIdentifier, typeArgs: [AptosTypeTag], args: [Data]) {
-        self.moduleName = moduleName
-        self.functionName = functionName
+    /// - Returns: Aptos Script
+    public init(code: Data, typeArgs: [AptosTypeTag], args: [AptosTransactionArgument]) {
+        self.code = code
         self.typeArgs = typeArgs
         self.args = args
     }
-    
-    public static func natural(module: String, func: String, typeArgs: [AptosTypeTag], args: [Data]) throws -> Self {
-        return AptosEntryFunction(moduleName: try AptosModuleId.fromString(module),
-                                   functionName: AptosIdentifier(`func`),
-                                   typeArgs: typeArgs,
-                                   args: args)
-    }
 }
 
-extension AptosEntryFunction: BorshCodable {
+extension AptosScript: BorshCodable {
     public init(from reader: inout BinaryReader) throws {
-        self.moduleName = try .init(from: &reader)
-        self.functionName = try .init(from: &reader)
+        self.code = try VarData.init(from: &reader).data
         self.typeArgs = try .init(from: &reader)
-        self.args = (try [VarData].init(from: &reader)).map({$0.data})
+        self.args = try .init(from: &reader)
     }
     
     public func serialize(to writer: inout Data) throws {
-        try moduleName.serialize(to: &writer)
-        try functionName.serialize(to: &writer)
+        try VarData(code).serialize(to: &writer)
         try typeArgs.serialize(to: &writer)
-        try args.map({VarData($0)}).serialize(to: &writer)
+        try args.serialize(to: &writer)
     }
 }
